ee5aeb3134ae37444c640fe4e477e2acc2f2c167
Alan Villa <a19i23@hotmail.com>
2020-08-19 17:19:42 -0700

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+function App() {
+  const people = ['John', 'Dave', 'Jane'];
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+  function handleInputChange(event) {
+    const inputValue = event.target.value;
+    console.log(inputValue);
+  }
+  return (
+    <ul>
+      {people.map((person, i) => (
+        <Person key={i} person={person} />
+      ))}
+      <input onChange={handleInputChange}></input>
+    </ul>
+  );
+}
+
+function Person(props) {
+  function handlePersonClick() {
+    alert(props.person);
+  }
+  return <li onClick={handlePersonClick}>{props.person}</li>;
+}
+
+const rootNode = document.getElementById('root');
+ReactDOM.render(<App />, rootNode);
